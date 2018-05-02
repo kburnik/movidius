f9690daf26d9101f40e12e4daabadb4f1b728741
Kristijan Burnik <kristijanburnik@gmail.com>
2018-05-02 22:31:53 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,2 @@
+# Movidius scripts
+Example scripts for the Movidius neural stick
---
+++ test_stick.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+from mvnc import mvncapi as mvnc
+import sys
+
+devices = mvnc.EnumerateDevices()
+
+if len(devices) == 0:
+  print("No devices were detected!")
+  sys.exit(1)
+
+print("Devices were found!")
+
+for device in devices:
+  print("Device", device)
+
+
